026ddcdf85efb2f6fe3957c00b6d4830b7a81b92
mikeforscutt <mikeforscutt@gmail.com>
2022-06-28 11:53:59 +0100

    added rectangle in random locations

--- src/js/script.js
+++ src/js/script.js
@@ -20,3 +20,41 @@ var food = new Box({
   height: 25,
   color: "#FEB249",
 });
+
+function Box(options) {
+  this.x = options.x || 10;
+  this.y = options.y || 10;
+  this.width = options.width || 40;
+  this.height = options.height || 50;
+  this.color = options.color || "#000000";
+  this.speed = options.speed || 5;
+  this.direction = options.direction || "right";
+  this.collideWith = function (otherobject) {
+    var myleft = this.x;
+    var myright = this.x + this.width;
+    var mytop = this.y;
+    var mybottom = this.y + this.height;
+    var otherleft = otherobject.x;
+    var otherright = otherobject.x + otherobject.width;
+    var othertop = otherobject.y;
+    var otherbottom = otherobject.y + otherobject.height;
+    var collision = true;
+    if (
+      mybottom <= othertop ||
+      mytop >= otherbottom ||
+      myright <= otherleft ||
+      myleft >= otherright
+    ) {
+      collision = false;
+    }
+    return collision;
+  };
+}
+
+function drawBox(box) {
+  ctx.fillStyle = box.color;
+  ctx.fillRect(box.x, box.y, box.width, box.height);
+}
+
+drawBox(player);
+drawBox(food);
