37c30f487bf8066e349098ee20dd6dff4c3b479d
mikeforscutt <mikeforscutt@gmail.com>
2022-06-29 17:45:59 +0100

    optimised code

--- src/js/script.js
+++ src/js/script.js
@@ -3,15 +3,17 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 500;
 
+let allItems = [];
 // An individual rectangle. Holds properties and behavior for one
 class Rectangle {
   constructor(options) {
     this.x = options.x || 10;
     this.y = options.y || 10;
-    this.width = options.width || 40;
+    this.width = options.width || 50;
     this.height = options.height || 50;
     this.color = options.color || "#000000";
     this.speed = options.speed || 5;
+
     this.collideWith = function (otherobject) {
       var myleft = this.x;
       var myright = this.x + this.width;
@@ -43,139 +45,98 @@ class Triangle {
   }
 }
 
-// Class that holds a collection of Rectangles and properties and functions for the group
-class Shapes {
-  constructor() {
-    this.Rectangles = [];
-    this.Triangles = [];
-  }
-  // create a new rectangle and save it in the collection
-  newRectangle() {
-    let newRect = new Rectangle({
-      x: Math.floor(Math.random() * (canvas.width - 25)),
-      y: Math.floor(Math.random() * 350),
-      width: 50,
-      height: 50,
-      color: "#FEB249",
-      speed: 5,
-    });
-
-    // checking collision box with list
-    let isCollision = false;
-    for (var i = 0; i < this.Rectangles.length; i++) {
-      let item = this.Rectangles[i];
-
-      // checking left-top
-      if (
-        item.x <= newRect.x &&
-        newRect.x <= item.x + item.width &&
-        item.y <= newRect.y &&
-        newRect.y <= item.y + item.height
-      ) {
-        isCollision = true;
-        break;
-      }
-      // checking right-top
-      if (
-        item.x <= newRect.x + newRect.width &&
-        newRect.x + newRect.width <= item.x + item.width &&
-        item.y <= newRect.y &&
-        newRect.y <= item.y + item.height
-      ) {
-        isCollision = true;
-        break;
-      }
-      // checking left-bottom
-      if (
-        item.x <= newRect.x &&
-        newRect.x <= item.x + item.width &&
-        item.y <= newRect.y + newRect.height &&
-        newRect.y + newRect.height <= item.y + item.height
-      ) {
-        isCollision = true;
-        break;
-      }
-      // checking right-bottom
-      if (
-        item.x <= newRect.x + newRect.width &&
-        newRect.x + newRect.width <= item.x + item.width &&
-        item.y <= newRect.y + newRect.height &&
-        newRect.y + newRect.height <= item.y + item.height
-      ) {
-        isCollision = true;
-        break;
-      }
-    }
-
-    if (isCollision) {
-      alert("collision =========");
-    }
-
-    this.Rectangles.push(newRect);
-
-    return newRect;
-  }
-  get allRectangles() {
-    return this.Rectangles;
-  }
-  get numberOfRectangles() {
-    return this.Rectangles.length;
-  }
-  newTriangle() {
-    let t = new Triangle({
-      x: Math.floor(Math.random() * (canvas.width - 25)),
-      y: Math.floor(Math.random() * 350),
-      fillColor: "#FEB249",
-    });
-    this.Triangles.push(t);
-    return t;
-  }
-  get allTriangles() {
-    return this.Triangles;
-  }
-  get numberOfTriangles() {
-    return this.Triangles.length;
+class Circle extends Rectangle {
+  constructor(options) {
+    super(options);
   }
 }
 
-let shape = new Shapes();
-shape.newRectangle();
-shape.newRectangle();
-shape.newRectangle();
+// Class that holds a collection of Rectangles and properties and functions for the group
+class Shape extends Rectangle {
+  constructor(options) {
+    super(options);
+  }
+}
+
+function checkCollision(newRect) {
+  let isCollision = false;
+
+  for (var i = 0; i < allItems.length; i++) {
+    let item = allItems[i];
+
+    if (item.collideWith(newRect)) {
+      isCollision = true;
+      break;
+    }
+
+    /*
+    // checking left-top
+    if (
+      item.x <= newRect.x &&
+      newRect.x <= item.x + item.width &&
+      item.y <= newRect.y &&
+      newRect.y <= item.y + item.height
+    ) {
+      isCollision = true;
+      break;
+    }
+    // checking right-top
+    if (
+      item.x <= newRect.x + newRect.width &&
+      newRect.x + newRect.width <= item.x + item.width &&
+      item.y <= newRect.y &&
+      newRect.y <= item.y + item.height
+    ) {
+      isCollision = true;
+      break;
+    }
+    // checking left-bottom
+    if (
+      item.x <= newRect.x &&
+      newRect.x <= item.x + item.width &&
+      item.y <= newRect.y + newRect.height &&
+      newRect.y + newRect.height <= item.y + item.height
+    ) {
+      isCollision = true;
+      break;
+    }
+    // checking right-bottom
+    if (
+      item.x <= newRect.x + newRect.width &&
+      newRect.x + newRect.width <= item.x + item.width &&
+      item.y <= newRect.y + newRect.height &&
+      newRect.y + newRect.height <= item.y + item.height
+    ) {
+      isCollision = true;
+      break;
+    }
+    */
+  }
+
+  if (isCollision) {
+    alert("collision =========");
+  }
+}
 
 function draw() {
-  // shape.Triangles.forEach(drawTri);
-
-  // let value = shape.Rectangles;
-  // console.log(value);
-
-  // if (shape.Rectangles.collideWith(shape.Rectangle)) {
-  //   ctx.font = "24px sans-serif";
-  //   ctx.fillStyle = "#002447";
-  //   ctx.fillText("COLLISION DETECTED", 50, 50);
-  // } else {
-  //   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  //   shape.Rectangles.forEach(drawBox);
-  // }
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  shape.Rectangles.forEach(drawBox);
+  allItems.forEach(drawBox);
 }
 
 function drawBox(box) {
   ctx.fillStyle = box.color;
-  ctx.fillRect(box.x, box.y, box.width, box.height);
+
+  // draw rectangle
+  if (box instanceof Shape) {
+    ctx.fillRect(box.x, box.y, box.width, box.height);
+  }
+  // draw circle
+  if (box instanceof Circle) {
+    ctx.beginPath();
+    ctx.arc(box.x, box.y, box.width / 2, 0, 2 * Math.PI, false);
+    ctx.fill();
+  }
 }
-// function drawTri(tri) {
-//   // Draw triangle
-//   ctx.beginPath();
-//   ctx.lineTo(0, 0);
-//   ctx.lineTo(tri.x, tri.x);
-//   ctx.lineTo(0, tri.y);
-//   ctx.closePath();
-//   // Fill color
-//   ctx.fillStyle = this.fillColor;
-//   ctx.fill();
-// }
 
 var form = document.getElementById("user_form");
 function handleForm(event) {
@@ -184,14 +145,38 @@ function handleForm(event) {
   let selectedShape = document.getElementById("shapes").value;
 
   if (selectedShape === "square") {
-    shape.newRectangle();
+    // if checked square
+    let shape = new Shape({
+      x: Math.floor(Math.random() * (canvas.width - 25)),
+      y: Math.floor(Math.random() * 350),
+      width: 50,
+      height: 50,
+      color: "#FEB249",
+      speed: 5,
+    });
+
+    checkCollision(shape);
+    allItems.push(shape);
+
     draw();
   } else if (selectedShape === "triangle") {
-    shape.newTriangle();
+    //shape.newTriangle();
     draw();
   } else {
+    // if checked 'circle'
+    let circle = new Circle({
+      x: Math.floor(Math.random() * (canvas.width - 25)),
+      y: Math.floor(Math.random() * 350),
+      width: 50,
+      height: 50,
+      color: "#FE0049",
+      speed: 5,
+    });
+
+    checkCollision(circle);
+    allItems.push(circle);
+
+    draw();
   }
 }
 form.addEventListener("submit", handleForm);
-
-draw();
